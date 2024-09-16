aaf72dd11af8fd69b7f58c6eb37349d0c1f40c90
Tanja-R <tkjtkr@protonmail.com>
2024-09-16 12:37:23 +0300

    Changed indetation and moved variables

--- src/Input/ComboInput.tsx
+++ src/Input/ComboInput.tsx
@@ -1,26 +1,28 @@
-const InputFieldBaseText:string = "Combo:"
-const InputFieldPlaceholder:string = "Type Combo Here!"
 type InputProps = {
   onModify: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
+};
+
 // Takes user input and passes it to be parsed.
 // Potentially reactively expand input field to accomodate larger
 // inputs.
-function Input({onModify} : InputProps) {
-    return (
-      <div className="Input">
-        <label className="block text-gray-200
+function Input({onModify}: InputProps) {
+  const InputFieldBaseText: string = "Combo:";
+  const InputFieldPlaceholder: string = "Type Combo Here!";
+
+  return (
+    <div className="Input">
+      <label className="block text-gray-200
         text-sm font-bold mb-2" htmlFor="combo">
-          {InputFieldBaseText}
-        </label>
-        <input className="shadow appearance-none bg-blue-200
+        {InputFieldBaseText}
+      </label>
+      <input className="shadow appearance-none bg-blue-200
         border rounded w-full py-2 px-3 text-gray-500
         leading-tight focus:outline-none focus:shadow-outline"
         id="combo" type="text" placeholder={InputFieldPlaceholder}
         onChange={onModify}>
-        </input>
-      </div>
-    );
+      </input>
+    </div>
+  );
 }
 
 export default Input;
