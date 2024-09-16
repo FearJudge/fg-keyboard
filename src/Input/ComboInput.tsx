c741e8d9afbfbd62434701778cc32de146a7e3d7
Tanja-R <tkjtkr@protonmail.com>
2024-09-16 11:48:39 +0300

    Fixed Input's parameter type
    The type of onModify is (event: React.ChangeEvent<HTMLInputElement>) => void. So it is a function. It is passed to Input as a property of an object (props).

--- src/Input/ComboInput.tsx
+++ src/Input/ComboInput.tsx
@@ -1,21 +1,21 @@
 const InputFieldBaseText:string = "Combo:"
 const InputFieldPlaceholder:string = "Type Combo Here!"
-
+type InputProps = {
+  onModify: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
 // Takes user input and passes it to be parsed.
 // Potentially reactively expand input field to accomodate larger
 // inputs.
-// TODO: Figure out why this gives a warning with type. It should be this type:
-// React.ChangeEvent<HTMLInputElement>
-function Input({onModify} : any) {
+function Input({onModify} : InputProps) {
     return (
       <div className="Input">
-        <label className="block text-gray-200 
+        <label className="block text-gray-200
         text-sm font-bold mb-2" htmlFor="combo">
           {InputFieldBaseText}
         </label>
         <input className="shadow appearance-none bg-blue-200
         border rounded w-full py-2 px-3 text-gray-500
-        leading-tight focus:outline-none focus:shadow-outline" 
+        leading-tight focus:outline-none focus:shadow-outline"
         id="combo" type="text" placeholder={InputFieldPlaceholder}
         onChange={onModify}>
         </input>
