a0a3ad2582e24f72c28f48c9cd905c8ac7dc195b
Ossian Johansson <ossian.johansson@gmail.com>
2024-09-17 09:51:06 +0300

    Testing SVG to output

--- src/Output/Output.tsx
+++ src/Output/Output.tsx
@@ -1,4 +1,4 @@
-
+import testimage from '../assets/Input_SVGs/1B_A.svg'
 
 // Displays the output gotten by the outputmapper.
 // Potentially also displays other input like additional data fields.
@@ -8,6 +8,7 @@ function Output({commands, buttonsToMap} : {commands: string[], buttonsToMap: nu
   return (
     <div className="Output">
       <h1> This is Output: </h1>
+      <img src={testimage}></img>
       <p> {buttonsToMap} </p>
       {
         commands.map((m, i) => {
