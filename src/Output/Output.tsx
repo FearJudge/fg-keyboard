d215a99ab93fcbb1bba3477867c08f6e8b57f977
Tanja-R <tkjtkr@protonmail.com>
2024-09-17 10:38:08 +0300

    Displayed canvas element with testImage

--- src/Output/Output.tsx
+++ src/Output/Output.tsx
@@ -5,10 +5,25 @@ import testimage from '../assets/Input_SVGs/1B_A.svg'
 // (Character, Combo Damage, Additional Notes etc.)
 function Output({commands, buttonsToMap} : {commands: string[], buttonsToMap: number[]}) {
   console.log(commands);
+  function myCanvas() {
+    const c : HTMLCanvasElement | null = document.getElementById("comboArea") as HTMLCanvasElement;
+    if (c !== null) {
+      const ctx = c.getContext("2d");
+      if (ctx !== null) {
+        const base_image = new Image(32, 32);
+        base_image.src = testimage;
+        ctx.drawImage(base_image, 10, 10);
+      }
+    }
+  }
+
   return (
     <div className="Output">
       <h1> This is Output: </h1>
       <img src={testimage}></img>
+      <div >
+      <canvas id="comboArea" style={{ color: 'blue',   backgroundColor: 'lightgray'}} onClick={myCanvas}></canvas>
+      </div>
       <p> {buttonsToMap} </p>
       {
         commands.map((m, i) => {
