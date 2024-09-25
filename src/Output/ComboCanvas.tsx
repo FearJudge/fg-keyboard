1a1aa98b84e44035a1cdf316fbc40c82bda79f44
Fear Judge <fearjudge_dev@outlook.com>
2024-09-25 18:35:51 +0300

    Initial Save Ability without Dialog

--- src/Output/ComboCanvas.tsx
+++ src/Output/ComboCanvas.tsx
@@ -1,13 +1,15 @@
 import { drawCombo } from './drawCombo';
+import ComboSaver from './OutputSaver';
 
 export default function ComboCanvas({ buttonsToMap }: {buttonsToMap: number[]}) {
   const { canvasWidth, canvasHeight } = drawCombo(buttonsToMap);
 
-  return <div className="grid">
+  return <div id="outputDisplay" className="grid">
     <canvas id="comboArea" className="
     self-baseline place-self-center
     bg-cyan-900 rounded-md"
     width={canvasWidth} height={canvasHeight}>
     </canvas>
+    <button onClick={ComboSaver} className="place-self-end mx-12 w-1/5 font-sans">Save Combo</button>
   </div>
 }
--- src/Output/OutputSaver.tsx
+++ src/Output/OutputSaver.tsx
@@ -1,2 +1,15 @@
 // Responsible for saving the image to a file that the user can download.
 // Medium/Low Priority
+
+export default function OpenSave(e: React.MouseEvent<HTMLButtonElement>)
+{
+  const anchor: HTMLAnchorElement = document.createElement("a");
+  anchor.href = (document.getElementById("comboArea") as HTMLCanvasElement).toDataURL("image/png");
+  const date = new Date();
+  const dateStr: string = "UnnamedCombo-" + date.getDay() + date.getMonth() + date.getFullYear() + "-" + date.getTime() + ".png";
+  anchor.download = dateStr;
+
+  document.getElementById("outputDisplay")?.appendChild(anchor);
+  anchor.click();
+  document.getElementById("outputDisplay")?.removeChild(anchor);
+}
